b1d89b80f5bbff4a5cd0781118f1e82a43c8cd16
Martin del Pino <119134824+dellpinos@users.noreply.github.com>
2023-05-31 20:05:14 -0300

    barra de navegacion

--- views/templates/header.php
+++ views/templates/header.php
@@ -17,3 +17,16 @@
     </div>
 
 </header>
+<div class="barra">
+    <div class="barra__contenido">
+        <a href="/">
+            <h2 class="barra__logo">&#60DevWebCamp/&gt</h2>
+        </a>
+        <nav class="navegacion">
+            <a href="/devwebcamp" class="navegacion__enlace">Evento</a>
+            <a href="/paquetes" class="navegacion__enlace">Paquetes</a>
+            <a href="/workshops-conferencias" class="navegacion__enlace">Workshop / Conferencias</a>
+            <a href="/registro" class="navegacion__enlace">Comprar Pase</a>
+        </nav>
+    </div>
+</div>
