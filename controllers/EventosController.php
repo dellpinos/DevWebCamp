df4b44ca445acb5588b0b668bdef4c92281a7c05
Martin del Pino <119134824+dellpinos@users.noreply.github.com>
2023-06-16 12:22:47 -0300

    Desarrollando formulario Evento

--- controllers/EventosController.php
+++ controllers/EventosController.php
@@ -2,7 +2,11 @@
 
 namespace Controllers;
 
+use Model\Dia;
+use Model\Hora;
 use MVC\Router;
+use Model\Evento;
+use Model\Categoria;
 
 class EventosController {
 
@@ -17,10 +21,33 @@ class EventosController {
     public static function crear(Router $router) {
 
         $alertas = [];
+        $categorias = Categoria::all('ASC');
+        $dias = Dia::all('ASC');
+        $horas = Hora::all('ASC');
+        $evento = new Evento;
+
+
+        if($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+            $evento->sincronizar($_POST);
+            $alertas = $evento->validar();
+
+            if(empty($alertas)) {
+                $resultado = $evento->guardar();
+                if($resultado) {
+                    header('Location: /admin/eventos');
+                }
+            }
+
+        }
 
         $router->render('admin/eventos/crear', [
             'titulo' => 'Registrar Evento',
-            'alertas' => $alertas
+            'alertas' => $alertas,
+            'categorias' => $categorias,
+            'dias' => $dias,
+            'horas' => $horas,
+            'evento' => $evento
         ]);
     }
 
--- views/admin/eventos/formulario.php
+++ views/admin/eventos/formulario.php
@@ -3,12 +3,57 @@
 
     <div class="formulario__campo">
         <label for="nombre" class="formulario__label">Nombre Evento</label>
-        <input type="text" class="formulario__input" id="nombre" name="nombre" placeholder="Nombre Evento">
+        <input type="text" class="formulario__input" id="nombre" name="nombre" placeholder="Nombre Evento" value="<?php echo $evento->nombre; ?>">
     </div>
 
     <div class="formulario__campo">
         <label for="descripcion" class="formulario__label">Descripción</label>
-        <textarea class="formulario__input" id="descripcion" name="descripcion" placeholder="Descripcion Evento" rows="7"></textarea>
+        <textarea class="formulario__input" id="descripcion" name="descripcion" placeholder="Descripcion Evento" rows="7"><?php echo $evento->descripcion; ?></textarea>
     </div>
 
+    <div class="formulario__campo">
+        <label for="categoria" class="formulario__label">Categoria o Tipo de Evento</label>
+        <select class="formulario__select" id="categoria" name="categoria_id">
+            <option value="" selected disabled>- Seleccionar -</option>
+            <?php foreach ($categorias as $categoria) : ?>
+                <option <?php echo ($evento->categoria_id === $categoria->id) ? 'selected' : ''; ?> value="<?php echo $categoria->id; ?>"><?php echo $categoria->nombre; ?></option>
+            <?php endforeach; ?>
+        </select>
+    </div>
+
+    <div class="formulario__campo">
+        <label for="descripcion" class="formulario__label">Selecciona el dia</label>
+        <div class="formulario__radio">
+            <?php foreach($dias as $dia) : ?>
+                <div>
+                    <label for="<?php echo strtolower($dia->nombre); ?>"><?php echo $dia->nombre; ?></label>
+                    <input type="radio" id="<?php echo strtolower($dia->nombre); ?>" name="dia" value="<?php echo $dia->id; ?>">
+                </div>
+            <?php endforeach; ?>
+        </div>
+    </div>
+
+    <div id="horas" class="formulario__campo">
+        <label class="formulario__label">Seleccionar Hora</label>
+        <ul class="horas">
+            <?php foreach($horas as $hora) : ?>
+                <li class="horas__hora"><?php echo $hora->hora; ?></li>
+            <?php endforeach; ?>
+        </ul>
+    </div>
+
+</fieldset>
+
+<fieldset class="formulario__fieldset">
+    <legend class="formulario__legend">Información Extra</legend>
+
+    <div class="formulario__campo">
+        <label for="ponentes" class="formulario__label">Conferencista</label>
+        <input type="text" class="formulario__input" id="ponentes" placeholder="Buscar Conferencista">
+    </div>
+
+    <div class="formulario__campo">
+        <label for="disponibles" class="formulario__label">Lugares Disponibles</label>
+        <input type="number" min="1" value="<?php echo $evento->disponibles; ?>" class="formulario__input" id="disponibles" name="disponibles" placeholder="Ej. 20">
+    </div>
 </fieldset>
