823b2aaf04477a94a8c78f38ef95a03f25246386
Martin del Pino <119134824+dellpinos@users.noreply.github.com>
2023-07-06 00:12:40 -0300

    Tabla regalos

--- views/registro/evento.php
+++ views/registro/evento.php
@@ -13,5 +13,13 @@
             </picture>
             <p class="evento__autor-nombre"><?php echo $evento->ponente->nombre . " " . $evento->ponente->apellido; ?></p>
         </div>
+
+        <button 
+        type="button" 
+        data-id="<?php echo $evento->id; ?>" 
+        class="evento__agregar"
+        <?php echo ($evento->disponibles === "0") ? 'disabled' : ''; ?>
+        ><?php echo ($evento->disponibles === "0") ? 'Agotado' : 'Agregar - ' . $evento->disponibles . ' Disponibles'; ?>
+        </button>
     </div>
 </div>
